fc5995bc30cc2eda1b45144e3c84e9dca90618a9
Mugen2411 <shojirintaro@gmail.comom>
2024-11-11 21:24:32 +0900

    add: ABC379がafterで引っかかったので正攻法で再討伐した

---
+++ ABC379/D2.cpp
@@ -0,0 +1,47 @@
+﻿#include <iostream>
+#include <queue>
+
+int main()
+{
+    int64_t Q;
+    std::cin >> Q;
+
+    std::queue<int64_t> inventory;
+    int64_t curTime = 0;
+    while (Q--)
+    {
+        int cmd;
+        std::cin >> cmd;
+        switch (cmd)
+        {
+        case 1:
+            inventory.push(curTime);
+            break;
+        case 2:
+        {
+            int64_t T;
+            std::cin >> T;
+            curTime += T;
+        }
+        break;
+        case 3:
+        {
+            int64_t H;
+            std::cin >> H;
+            int64_t ans = 0;
+            while (!inventory.empty() && curTime - inventory.front() >= H)
+            {
+                ++ans;
+                inventory.pop();
+            }
+            std::cout << ans << std::endl;
+        }
+        break;
+
+        default:
+            break;
+        }
+    }
+
+    return 0;
+}
