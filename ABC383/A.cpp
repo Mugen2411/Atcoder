b3439655fc34f99e0430025a6af44848c10737e1
Mugen2411 <shojirintaro@gmail.comom>
2024-12-07 23:30:17 +0900

    add: ABC383終了。2完だけど-10で済んだ。。。

--- ABC383/A.cpp
+++ ABC383/A.cpp
@@ -2,5 +2,19 @@
 
 int main()
 {
+    int N;
+    std::cin >> N;
+    int last = 0;
+    int cur = 0;
+    while (N--)
+    {
+        int T, V;
+        std::cin >> T >> V;
+        cur = std::max(0, cur - T + last);
+        cur += V;
+        last = T;
+        std::cerr << cur << std::endl;
+    }
+    std::cout << cur;
     return 0;
 }
