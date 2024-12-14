7e896b4e2ce56327f97f65996b6ca8ff8f4f72a5
Mugen2411 <shojirintaro@gmail.comom>
2024-12-14 20:56:03 +0900

    add: BinarySearchをmoduleに追加

---
+++ module/BinarySearch.cpp
@@ -0,0 +1,25 @@
+﻿#ifndef ___INCLUDED_BINARY_SEARCH___
+#define ___INCLUDED_BINARY_SEARCH___
+
+#include <cstdint>
+
+template <class F>
+int64_t BinarySearch(int64_t ng, int64_t ok, F comp)
+{
+    int64_t mid;
+    while (std::abs(ng - ok) > 1)
+    {
+        mid = (ng + ok) / 2;
+        if (comp(mid))
+        {
+            ok = mid;
+        }
+        else
+        {
+            ng = mid;
+        }
+    }
+    return ok;
+}
+
+#endif //___INCLUDED_BINARY_SEARCH___
