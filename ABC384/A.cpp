b6713e2124ad6f5564a18dcbd3545e557ac244d4
Mugen2411 <shojirintaro@gmail.comom>
2024-12-15 00:01:56 +0900

    add: ABC384終了

--- ABC384/A.cpp
+++ ABC384/A.cpp
@@ -1,6 +1,21 @@
 ﻿#include <iostream>
+#include <string>
 
 int main()
 {
+    int N;
+    char c1, c2;
+    std::string S;
+    std::cin >> N >> c1 >> c2;
+    std::cin >> S;
+
+    for (int i = 0; i < N; ++i)
+    {
+        if (S[i] != c1)
+        {
+            S[i] = c2;
+        }
+    }
+    std::cout << S;
     return 0;
 }
--- ABC384/C.cpp
+++ ABC384/C.cpp
@@ -1,6 +1,40 @@
 ﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
 
 int main()
 {
+    char tag[5] = {'A', 'B', 'C', 'D', 'E'};
+    int64_t score[5];
+    std::cin >> score[0] >> score[1] >> score[2] >> score[3] >> score[4];
+
+    std::vector<std::pair<std::string, int64_t>> vec(32);
+
+    for (int i = 0; i < 32; ++i)
+    {
+        int scr = 0;
+        std::string name;
+        for (int j = 0; j < 5; ++j)
+        {
+            if (i & (1 << j))
+            {
+                scr += score[j];
+                name += tag[j];
+            }
+        }
+        vec[i].first = name;
+        vec[i].second = scr;
+    }
+
+    std::stable_sort(vec.begin(), vec.end(), [](const std::pair<std::string, int64_t> &lhs, const std::pair<std::string, int64_t> &rhs)
+                     { return lhs.first < rhs.first; });
+    std::stable_sort(vec.begin(), vec.end(), [](const std::pair<std::string, int64_t> &lhs, const std::pair<std::string, int64_t> &rhs)
+                     { return lhs.second > rhs.second; });
+
+    for (auto &i : vec)
+    {
+        std::cout << i.first << std::endl;
+    }
     return 0;
 }
