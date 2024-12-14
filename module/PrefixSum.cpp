5850e0ad0035a7306ddc66cbb2f50fd161fef950
Mugen2411 <shojirintaro@gmail.comom>
2024-12-15 00:34:39 +0900

    add: moduleに追加

---
+++ module/PrefixSum.cpp
@@ -0,0 +1,56 @@
+﻿#ifndef ___INCLUDED_PREFIX_SUM___
+#define ___INCLUDED_PREFIX_SUM___
+
+#include <vector>
+#include <algorithm>
+#include <iterator>
+#include <cassert>
+
+/**
+    @brief        累積和
+
+    @tparam     Iterator   範囲指定に使用するイテレータの型
+ */
+template <class Iterator>
+class PrefixSum
+{
+public:
+    using T = typename std::iterator_traits<Iterator>::value_type; //!< 格納する型
+
+    /**
+        @brief    コンストラクタ
+
+        @param[in]    rawData     元となるデータ
+    */
+    PrefixSum(Iterator beginItr, Iterator endItr)
+        : m_data()
+    {
+        int size = std::distance(beginItr, endItr);
+        m_data.resize(size + 1, 0);
+        int cur = 0;
+        for (decltype(beginItr) itr = beginItr; itr != endItr; itr = std::next(itr))
+        {
+            m_data[cur + 1] = m_data[cur] + *itr;
+            ++cur;
+        }
+    }
+
+    /**
+        @brief        累積和の取得
+
+        @param[in]    targetIdx   加算を終了するインデックス
+        @param[in]    offsetIdx   加算を開始するインデックス
+        @return                 累積和
+     */
+    T GetSum(size_t targetIdx, size_t offsetIdx)
+    {
+        assert(0 <= targetIdx && targetIdx < m_data.size());
+        assert(0 <= offsetIdx && offsetIdx < m_data.size());
+        return m_data[targetIdx] - m_data[offsetIdx];
+    }
+
+private:
+    std::vector<T> m_data; //!< データ構造
+};
+
+#endif //___INCLUDED_PREFIX_SUM___
