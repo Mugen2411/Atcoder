556e2a1b6416f9de28d4e8bb50c97c4b172202f6
Mugen2411 <shojirintaro@gmail.comom>
2025-01-04 20:46:18 +0900

    add: ABC386終了

--- ABC386/A.cpp
+++ ABC386/A.cpp
@@ -1,6 +1,23 @@
 ﻿#include <iostream>
+#include <set>
 
 int main()
 {
+    std::set<int> s;
+    for (int i = 0; i < 4; ++i)
+    {
+        int tmp;
+        std::cin >> tmp;
+        s.insert(tmp);
+    }
+    if (s.size() == 2)
+    {
+        std::cout << "Yes";
+        return 0;
+    }
+    else
+    {
+        std::cout << "No";
+    }
     return 0;
 }
--- ABC386/C.cpp
+++ ABC386/C.cpp
@@ -1,6 +1,50 @@
 ﻿#include <iostream>
+#include <string>
 
 int main()
 {
+    int64_t K;
+    std::string S, T;
+    std::cin >> K >> S >> T;
+    if (std::abs((int64_t)S.size() - (int64_t)T.size()) >= 2)
+    {
+        std::cout << "No";
+        return 0;
+    }
+    if (S.size() > T.size())
+    {
+        std::swap(S, T);
+    }
+
+    int offset = 0;
+    int edited = 0;
+    for (int i = 0; i < S.size(); ++i)
+    {
+        if (S[i] == T[i + offset])
+        {
+            continue;
+        }
+        if (S[i] == T[i + offset + 1])
+        {
+            offset++;
+            edited++;
+            continue;
+        }
+        if (S[i + 1] == T[i + offset + 1])
+        {
+            edited++;
+            continue;
+        }
+        std::cout << "No";
+        return 0;
+    }
+    if (edited <= 1)
+    {
+        std::cout << "Yes";
+    }
+    else
+    {
+        std::cout << "No";
+    }
     return 0;
 }
