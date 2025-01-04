f99c6a7d51a5a2fa5d19698a8a653e2574cc8af1
Mugen2411 <shojirintaro@gmail.comom>
2025-01-04 23:59:42 +0900

    add: ABC387終了。pf1109でrate794(+41)

--- ABC387/A.cpp
+++ ABC387/A.cpp
@@ -2,5 +2,8 @@
 
 int main()
 {
+    int A, B;
+    std::cin >> A >> B;
+    std::cout << (A + B) * (A + B);
     return 0;
 }
--- ABC387/B.cpp
+++ ABC387/B.cpp
@@ -2,5 +2,19 @@
 
 int main()
 {
+    int X;
+    std::cin >> X;
+    int ans = 0;
+    for (int i = 1; i <= 9; ++i)
+    {
+        for (int j = 1; j <= 9; ++j)
+        {
+            if (i * j != X)
+            {
+                ans += i * j;
+            }
+        }
+    }
+    std::cout << ans;
     return 0;
 }
