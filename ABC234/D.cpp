89dea0c410163a4a3ecae0e50676791e61b7a121
Mugen2411 <shojirintaro@gmail.comom>
2025-01-12 01:47:26 +0900

    add: ABC234-D精進

---
+++ ABC234/D.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <queue>
+#include <vector>
+
+template <typename T>
+using greater_priority_queue = std::priority_queue<T, std::vector<T>, std::greater<T>>;
+
+int main()
+{
+    int64_t N, K;
+    std::cin >> N >> K;
+    std::vector<int64_t> P(N);
+    for (int i = 0; i < N; ++i)
+    {
+        std::cin >> P[i];
+    }
+    greater_priority_queue<int64_t> q;
+    for (int k = 0; k < K; ++k)
+    {
+        q.push(P[k]);
+    }
+    std::cout << q.top() << std::endl;
+
+    for (int k = K; k < N; ++k)
+    {
+        if (q.top() < P[k])
+        {
+            q.pop();
+            q.push(P[k]);
+        }
+        std::cout << q.top() << std::endl;
+    }
+    return 0;
+}
