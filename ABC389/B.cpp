bff56772caa9f009acd6ef2cd056d3aac9cb9f29
Mugen2411 <shojirintaro@gmail.comom>
2025-01-19 00:01:19 +0900

    add: ABC389終了。pf1027でrt817(+26)!!!緑!!!

--- ABC389/B.cpp
+++ ABC389/B.cpp
@@ -2,5 +2,17 @@
 
 int main()
 {
+    int64_t N;
+    std::cin >> N;
+    int64_t ans = 1;
+    for (int i = 2;; ++i)
+    {
+        ans *= i;
+        if (N == ans)
+        {
+            std::cout << i;
+            return 0;
+        }
+    }
     return 0;
 }
--- ABC389/C.cpp
+++ ABC389/C.cpp
@@ -1,6 +1,45 @@
 ﻿#include <iostream>
+#include <vector>
 
 int main()
 {
+    int64_t Q;
+    std::cin >> Q;
+    int64_t dead = 0;
+    std::vector<int64_t> v;
+    v.push_back(0);
+    int64_t cur = 0;
+    while (Q--)
+    {
+        int cmd;
+        std::cin >> cmd;
+        switch (cmd)
+        {
+        case 1:
+        {
+            int64_t l;
+            std::cin >> l;
+            {
+                v.push_back(v.back() + l);
+            }
+        }
+        break;
+        case 2:
+        {
+            ++cur;
+            dead = v[cur];
+        }
+        break;
+        case 3:
+        {
+            int64_t k;
+            std::cin >> k;
+            --k;
+            std::cout << v[k + cur] - dead << std::endl;
+        }
+        default:
+            break;
+        }
+    }
     return 0;
 }
