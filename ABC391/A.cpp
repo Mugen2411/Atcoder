f37d470fa642fbebda270a7ebdba7211839c2d2f
Mugen2411 <shojirintaro@gmail.comom>
2025-02-01 23:26:43 +0900

    add: ABC391終了。pf916でrt801(+14)。緑に帰還

--- ABC391/A.cpp
+++ ABC391/A.cpp
@@ -1,6 +1,31 @@
 ﻿#include <iostream>
+#include <string>
 
 int main()
 {
+    std::string D;
+    std::cin >> D;
+    for (auto &c : D)
+    {
+        switch (c)
+        {
+        case 'N':
+            c = 'S';
+            break;
+
+        case 'S':
+            c = 'N';
+            break;
+
+        case 'E':
+            c = 'W';
+            break;
+
+        case 'W':
+            c = 'E';
+            break;
+        }
+    }
+    std::cout << D;
     return 0;
 }
--- ABC391/C.cpp
+++ ABC391/C.cpp
@@ -1,6 +1,54 @@
 ﻿#include <iostream>
+#include <vector>
 
 int main()
 {
+    int N, Q;
+    std::cin >> N >> Q;
+    std::vector<int> P(N);
+    std::vector<int> H(N);
+
+    for (int i = 0; i < N; ++i)
+    {
+        P[i] = i;
+        H[i] = 1;
+    }
+
+    int ans = 0;
+
+    while (Q--)
+    {
+        int cmd;
+        std::cin >> cmd;
+        switch (cmd)
+        {
+        case 1:
+        {
+            int p, h;
+            std::cin >> p >> h;
+            --p, --h;
+            if (H[P[p]] == 2)
+            {
+                --ans;
+            }
+            --H[P[p]];
+            P[p] = h;
+            ++H[P[p]];
+            if (H[P[p]] == 2)
+            {
+                ++ans;
+            }
+        }
+        break;
+        case 2:
+        {
+            std::cout << ans << std::endl;
+        }
+        break;
+
+        default:
+            break;
+        }
+    }
     return 0;
 }
