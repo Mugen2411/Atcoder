4b20f46462ad3989717af544c2aa5c8b1e4f5193
Mugen2411 <shojirintaro@gmail.comom>
2025-05-10 23:39:54 +0900

    add: ABC405終了。pf814でrt756(+7)

--- ABC405/A.cpp
+++ ABC405/A.cpp
@@ -2,5 +2,24 @@
 
 int main()
 {
+    int R, X;
+    std::cin >> X >> R;
+    if (R == 1)
+    {
+        if (1600 <= X && X < 3000)
+        {
+            std::cout << "Yes";
+            return 0;
+        }
+    }
+    else
+    {
+        if (1200 <= X && X < 2400)
+        {
+            std::cout << "Yes";
+            return 0;
+        }
+    }
+    std::cout << "No";
     return 0;
 }
