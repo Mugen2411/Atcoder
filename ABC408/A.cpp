ee27f85a7078b17fd2e20bb5a63b0716ff0db534
Mugen2411 <shojirintaro@gmail.com>
2025-06-05 22:14:33 +0900

    ABC408終了

--- ABC408/A.cpp
+++ ABC408/A.cpp
@@ -2,5 +2,20 @@
 
 int main()
 {
+    int N, S;
+    std::cin >> N >> S;
+    int last = 0;
+    while (N--)
+    {
+        int T;
+        std::cin >> T;
+        if (T - last > S)
+        {
+            std::cout << "No";
+            return 0;
+        }
+        last = T;
+    }
+    std::cout << "Yes";
     return 0;
 }
--- ABC408/D.cpp
+++ ABC408/D.cpp
@@ -1,6 +1,48 @@
 ﻿#include <iostream>
+#include <vector>
+#include <string>
 
 int main()
 {
+    int T;
+    std::cin >> T;
+    while (T--)
+    {
+        int N;
+        std::string S;
+        std::cin >> N >> S;
+        std::vector<int64_t> numZero(N + 1);
+        std::vector<int64_t> numOne(N + 1);
+        for (int i = 0; i < N; ++i)
+        {
+            if (S[i] == '0')
+            {
+                numZero[i + 1] = 1;
+            }
+            else
+            {
+                numOne[i + 1] = 1;
+            }
+        }
+        for (int i = 1; i <= N; ++i)
+        {
+            numZero[i] += numZero[i - 1];
+            numOne[i] += numOne[i - 1];
+        }
+        int64_t score = 9999999;
+        for (int i = 1; i <= N; ++i)
+        {
+            {
+                int64_t cur = (numZero[i - 1] - numZero[0]) + (numOne[N] - numOne[i]);
+                score = std::min(score, cur);
+            }
+            {
+                int64_t cur = (numOne[i - 1] - numOne[0]) + (numZero[N] - numZero[i]);
+                score = std::min(score, cur);
+            }
+        }
+
+        std::cout << score << std::endl;
+    }
     return 0;
 }
