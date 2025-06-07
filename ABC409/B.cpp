40b06b5fcf1d970d30893114ced568ffa54f0ea0
Mugen2411 <shojirintaro@gmail.com>
2025-06-07 23:35:50 +0900

    ABC409終了。pf442でrt714(-27)。

--- ABC409/B.cpp
+++ ABC409/B.cpp
@@ -1,6 +1,28 @@
 ﻿#include <iostream>
+#include <vector>
 
 int main()
 {
+    int N;
+    std::cin >> N;
+    std::vector<int64_t> mp(N + 1);
+    for (int i = 0; i < N; ++i)
+    {
+        int tmp;
+        std::cin >> tmp;
+        for (int j = std::min(tmp, N); j > 0; --j)
+        {
+            ++mp[j];
+        }
+    }
+    int ans = 0;
+    for (int i = 0; i <= N; ++i)
+    {
+        if (mp[i] >= i)
+        {
+            ans = std::max(ans, i);
+        }
+    }
+    std::cout << ans;
     return 0;
 }
