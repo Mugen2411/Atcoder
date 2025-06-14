5be45d8a049109b7381f1f9ba738fa58c96de0f0
Mugen2411 <shojirintaro@gmail.com>
2025-06-15 00:21:50 +0900

    ABC410終了。pf1026のrt750(+36)。4完嬉しい！！

--- ABC410/C.cpp
+++ ABC410/C.cpp
@@ -1,6 +1,46 @@
 ﻿#include <iostream>
+#include <vector>
 
 int main()
 {
+    int N, Q;
+    std::cin >> N >> Q;
+    std::vector<int> A(N);
+    for (int i = 0; i < N; ++i)
+    {
+        A[i] = i + 1;
+    }
+    int top = 0;
+    while (Q--)
+    {
+        int cmd;
+        std::cin >> cmd;
+        switch (cmd)
+        {
+        case 1:
+        {
+            int p, x;
+            std::cin >> p >> x;
+            A[(top + p - 1) % N] = x;
+        }
+        break;
+        case 2:
+        {
+            int p;
+            std::cin >> p;
+            std::cout << A[(top + p - 1) % N] << " ";
+            std::cout << std::endl;
+        }
+        break;
+        case 3:
+        {
+            int p;
+            std::cin >> p;
+            top += p;
+            top %= N;
+        }
+        break;
+        }
+    }
     return 0;
 }
--- ABC410/D.cpp
+++ ABC410/D.cpp
@@ -1,6 +1,52 @@
 ﻿#include <iostream>
+#include <vector>
+#include <unordered_set>
 
 int main()
 {
+    int N, M;
+    std::cin >> N >> M;
+    std::vector<std::unordered_set<int>> v(N);
+    struct EDGE
+    {
+        int weight;
+        int to;
+    };
+    std::vector<std::vector<EDGE>> graph(N);
+    while (M--)
+    {
+        int A, B, W;
+        std::cin >> A >> B >> W;
+        --A, --B;
+        graph[A].push_back({W, B});
+    }
+
+    auto dfs = [&](auto self, int idx, int cur)
+    {
+        if (v[idx].count(cur) != 0)
+        {
+            return;
+        }
+        v[idx].insert(cur);
+        for (auto &e : graph[idx])
+        {
+            self(self, e.to, cur ^ e.weight);
+        }
+    };
+
+    dfs(dfs, 0, 0);
+
+    if (v[N - 1].size() == 0)
+    {
+        std::cout << -1;
+        return 0;
+    }
+
+    int ans = 999999999;
+    for (auto a : v[N - 1])
+    {
+        ans = std::min(ans, a);
+    }
+    std::cout << ans;
     return 0;
 }
