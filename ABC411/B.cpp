fd218a7094e9396fbb9fb9b1d996533b08d9abec
Mugen2411 <shojirintaro@gmail.com>
2025-06-21 23:52:49 +0900

    ABC411終了。pf321(←!!!?!!???!?)。rt713(-37)

--- ABC411/B.cpp
+++ ABC411/B.cpp
@@ -1,6 +1,25 @@
 ﻿#include <iostream>
+#include <vector>
 
 int main()
 {
+    int N;
+    std::cin >> N;
+    std::vector<int> D(N - 1);
+    for (auto &&d : D)
+    {
+        std::cin >> d;
+    }
+    for (int i = 0; i < N - 1; ++i)
+    {
+        int cur = D[i];
+        for (int j = i + 1; j < N; ++j)
+        {
+            std::cout << cur << " ";
+            cur += D[j];
+        }
+        std::cout << std::endl;
+    }
+
     return 0;
 }
