81abe3aff82adfa6edf8e8e6902c242c5ed6173b
Mugen2411 <shojirintaro@gmail.com>
2025-06-29 02:33:37 +0900

    ABC412終了。pf500のrt693(-20)。ついに線が減った・・・

--- ABC412/A.cpp
+++ ABC412/A.cpp
@@ -2,5 +2,18 @@
 
 int main()
 {
+    int N;
+    std::cin >> N;
+    int ans = 0;
+    while (N--)
+    {
+        int A, B;
+        std::cin >> A >> B;
+        if (A < B)
+        {
+            ++ans;
+        }
+    }
+    std::cout << ans;
     return 0;
 }
--- ABC412/C.cpp
+++ ABC412/C.cpp
@@ -1,6 +1,72 @@
 ﻿#include <iostream>
+#include <vector>
+#include <algorithm>
 
 int main()
 {
+    int T;
+    std::cin >> T;
+    while (T--)
+    {
+        int N;
+        std::cin >> N;
+        std::vector<int64_t> S(N - 1);
+        int64_t cur;
+        for (auto &&s : S)
+        {
+            std::cin >> s;
+        }
+        int64_t goal;
+        std::cin >> goal;
+
+        std::sort(S.begin() + 1, S.end());
+
+        int64_t ans = 2;
+        bool success = false;
+        cur = S[0];
+        for (int i = 0; i < S.size(); ++i)
+        {
+            if (cur * 2 >= goal)
+            {
+                success = true;
+                std::cout << ans << std::endl;
+                break;
+            }
+
+            if (i != S.size() - 1 && S[i + 1] > cur * 2)
+            {
+                if (cur * 2 < S[i])
+                {
+                    break;
+                }
+                if (i != 0 || S[i - 1] != S[i])
+                {
+                    cur = S[i];
+                    ++ans;
+                }
+            }
+            else if (i == S.size() - 1)
+            {
+                if (S[i] * 2 >= goal)
+                {
+                    ++ans;
+                    success = true;
+                    std::cout << ans << std::endl;
+                }
+                else
+                {
+                    break;
+                }
+            }
+        }
+        if (!success)
+        {
+            std::cout << -1 << std::endl;
+        }
+        else
+        {
+            continue;
+        }
+    }
     return 0;
 }
