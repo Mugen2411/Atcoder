a005731129c85b88566704f657f8b45c5751d508
Mugen2411 <shojirintaro@gmail.com>
2025-07-05 23:41:13 +0900

    ABC413終了。pf955のrt722(+29)！今回は気持ちよくDが解けた

--- ABC413/A.cpp
+++ ABC413/A.cpp
@@ -2,5 +2,22 @@
 
 int main()
 {
+    int N, M;
+    std::cin >> N >> M;
+    int sum = 0;
+    while (N--)
+    {
+        int tmp;
+        std::cin >> tmp;
+        sum += tmp;
+    }
+    if (sum <= M)
+    {
+        std::cout << "Yes";
+    }
+    else
+    {
+        std::cout << "No";
+    }
     return 0;
 }
--- ABC413/C.cpp
+++ ABC413/C.cpp
@@ -1,6 +1,57 @@
 ﻿#include <iostream>
+#include <list>
 
 int main()
 {
+    struct ELEM
+    {
+        int64_t value;
+        int64_t num;
+    };
+    std::list<ELEM> ls;
+    int Q;
+    std::cin >> Q;
+    while (Q--)
+    {
+        int cmd;
+        std::cin >> cmd;
+        switch (cmd)
+        {
+        case 1:
+        {
+            int64_t c, x;
+            std::cin >> c >> x;
+            ls.push_back(ELEM{x, c});
+        }
+        break;
+
+        case 2:
+        {
+            int64_t k;
+            std::cin >> k;
+            int64_t ans = 0;
+            while (k > 0)
+            {
+                ELEM &top = ls.front();
+                if (top.num <= k)
+                {
+                    ans += top.value * top.num;
+                    k -= top.num;
+                    ls.pop_front();
+                }
+                else
+                {
+                    ans += top.value * k;
+                    top.num -= k;
+                    k = 0;
+                }
+            }
+            std::cout << ans << std::endl;
+        }
+        break;
+        default:
+            break;
+        }
+    }
     return 0;
 }
