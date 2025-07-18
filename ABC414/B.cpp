c6b7602eb5109765ae6e69b1312ebf552a892eca
Mugen2411 <shojirintaro@gmail.com>
2025-07-19 03:09:38 +0900

    add: マルチケースじゃない時に使えることも確認するために1問移植

--- ABC414/B.cpp
+++ ABC414/B.cpp
@@ -1,39 +1,168 @@
 ﻿#include <iostream>
 #include <string>
+#include <sstream>
+
 #include <vector>
 
+// #define ENABLE_MULTICASE //!< マルチケース用スイッチ：コメントを外すとマルチケースになる
+
+/**
+    @brief	Atcoderの解答を行うのに便利なクラス
+ */
+class AtcoderSolveHelper
+{
+private:
+    //----------- 編集エリア -----------------------------
+    /**
+        @brief	実際に問題を解く関数
+        @note   マルチケースの場合はケースごとに呼ばれます
+     */
+    void Solve()
+    {
+        struct ELEM
+        {
+            char c;
+            int64_t l;
+        };
+
+        int N;
+        In() >> N;
+
+        std::vector<ELEM> leng(N);
+        int64_t len = 0;
+        for (int i = 0; i < N; ++i)
+        {
+            char c;
+            int64_t l;
+            In() >> c >> l;
+            leng[i] = ELEM{c, l};
+            len += l;
+            if (len > 100)
+            {
+                Out() << "Too Long";
+                return;
+            }
+        }
+        for (int i = 0; i < N; ++i)
+        {
+            for (int j = 0; j < leng[i].l; ++j)
+            {
+                Out() << leng[i].c;
+            }
+        }
+    }
+
+    //----------- 以下編集の必要なし ----------------------
+
+public:
+    /**
+        @brief	各種処理の起点になる窓口
+     */
+    void Run()
+    {
+        int64_t numTestCases = 1;
+#ifdef ENABLE_MULTICASE
+        In() >> numTestCases;
+#endif
+        while (numTestCases--)
+        {
+            Solve();
+        }
+
+        std::cout << Out().str();
+    }
+
+private:
+    /**
+        @brief	対象とするコンテナの各要素に入力を受け取る
+
+        @tparam	TargetContainerType 対象とするコンテナの型
+        @param[in]	targetContainer 対象とするコンテナ
+     */
+    template <class TargetContainerType>
+    void EachInput(TargetContainerType &targetContainer)
+    {
+        for (auto &&target : targetContainer)
+        {
+            In() >> target;
+        }
+    }
+
+    /**
+        @brief	入力ストリームを取得
+
+        @return 入力ストリーム
+     */
+    std::istream &In()
+    {
+        return std::cin;
+    }
+
+    /**
+        @brief	出力ストリームを取得
+
+        @return 出力ストリーム
+     */
+    std::stringstream &Out()
+    {
+        return m_outStream;
+    }
+
+    /**
+        @brief	エラー出力ストリームを取得
+
+        @return エラー出力ストリーム
+     */
+    std::ostream &Error()
+    {
+        return std::cerr;
+    }
+
+    /**
+        @brief	Yesと1行出力する
+     */
+    void Yes()
+    {
+        Out() << "Yes" << std::endl;
+    }
+
+    /**
+        @brief	Noと1行出力する
+     */
+    void No()
+    {
+        Out() << "No" << std::endl;
+    }
+
+    /**
+        @brief	渡されたbool値に応じてYesかNoを出力する
+
+        @param[in]	isYes Yesを出力するかどうか
+     */
+    void YesNo(bool isYes)
+    {
+        if (isYes)
+        {
+            Yes();
+        }
+        else
+        {
+            No();
+        }
+    }
+
+private:
+    std::stringstream m_outStream; //!< 出力を一時的に貯めておけるストリーム
+};
+
+/**
+    @brief	エントリポイント
+
+    @return ステータスコード(atcoderにおいては基本0)
+ */
 int main()
 {
-    struct ELEM
-    {
-        char c;
-        int64_t l;
-    };
-
-    int N;
-    std::cin >> N;
-
-    std::vector<ELEM> leng(N);
-    int64_t len = 0;
-    for (int i = 0; i < N; ++i)
-    {
-        char c;
-        int64_t l;
-        std::cin >> c >> l;
-        leng[i] = ELEM{c, l};
-        len += l;
-        if (len > 100)
-        {
-            std::cout << "Too Long";
-            return 0;
-        }
-    }
-    for (int i = 0; i < N; ++i)
-    {
-        for (int j = 0; j < leng[i].l; ++j)
-        {
-            std::cout << leng[i].c;
-        }
-    }
+    AtcoderSolveHelper solver;
+    solver.Run();
     return 0;
 }
