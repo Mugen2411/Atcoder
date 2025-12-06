3e415743359339c6b0c6154aa4cf1e1806a59938
Mugen2411 <shojirintaro@gmail.com>
2025-12-07 04:58:01 +0900

    style: clang-formatを本格導入

--- ABC319/D.cpp
+++ ABC319/D.cpp
@@ -1,6 +1,6 @@
 ﻿#include <iostream>
-#include <string>
 #include <sstream>
+#include <string>
 #include <vector>
 
 // #define ENABLE_MULTICASE //!< マルチケース用スイッチ：コメントを外すとマルチケースになる
@@ -36,7 +36,7 @@ int64_t BinarySearch(int64_t ng, int64_t ok, F comp)
  */
 class AtcoderSolveHelper
 {
-private:
+  private:
     //----------- 編集エリア -----------------------------
     /**
         @brief	実際に問題を解く関数
@@ -49,35 +49,41 @@ private:
         std::vector<int64_t> L(N);
         EachInput(L);
 
-        int64_t ans = BinarySearch(-1, 1ll << 60, [&](int64_t cur)
-                                   {
+        int64_t ans = BinarySearch(-1, 1ll << 60, [&](int64_t cur) {
             int curIdx = 0;
-            for(int m=0; m<M; ++m){
+            for (int m = 0; m < M; ++m)
+            {
                 int64_t length = 0;
-                while(length < cur){
-                    if(cur - length >= L[curIdx] ){
+                while (length < cur)
+                {
+                    if (cur - length >= L[curIdx])
+                    {
                         length += L[curIdx] + 1;
                         curIdx++;
-                        if(curIdx == N){
+                        if (curIdx == N)
+                        {
                             break;
                         }
-                    }else{
+                    }
+                    else
+                    {
                         break;
                     }
                 }
-                if(curIdx == N)
+                if (curIdx == N)
                 {
                     break;
                 }
             }
-            return curIdx == N; });
+            return curIdx == N;
+        });
 
         Out() << ans;
     }
 
     //----------- 以下編集の必要なし ----------------------
 
-public:
+  public:
     /**
         @brief	各種処理の起点になる窓口
      */
@@ -95,7 +101,7 @@ public:
         std::cout << Out().str();
     }
 
-private:
+  private:
     /**
         @brief	対象とするコンテナの各要素に入力を受け取る
 
@@ -174,7 +180,7 @@ private:
         }
     }
 
-private:
+  private:
     std::stringstream m_outStream; //!< 出力を一時的に貯めておけるストリーム
 };
 
