be80e39c8ee1c973b19e86312fca22a600e0c600
Mugen2411 <shojirintaro@gmail.com>
2025-12-07 02:44:17 +0900

    train: ABC319-D

---
+++ ABC319/D.cpp
@@ -0,0 +1,191 @@
+﻿#include <iostream>
+#include <string>
+#include <sstream>
+#include <vector>
+
+// #define ENABLE_MULTICASE //!< マルチケース用スイッチ：コメントを外すとマルチケースになる
+
+#ifndef ___INCLUDED_BINARY_SEARCH___
+#define ___INCLUDED_BINARY_SEARCH___
+
+#include <cstdint>
+
+template <class F>
+int64_t BinarySearch(int64_t ng, int64_t ok, F comp)
+{
+    int64_t mid;
+    while (std::abs(ng - ok) > 1)
+    {
+        mid = (ng + ok) / 2;
+        if (comp(mid))
+        {
+            ok = mid;
+        }
+        else
+        {
+            ng = mid;
+        }
+    }
+    return ok;
+}
+
+#endif //___INCLUDED_BINARY_SEARCH___
+
+/**
+    @brief	Atcoderの解答を行うのに便利なクラス
+ */
+class AtcoderSolveHelper
+{
+private:
+    //----------- 編集エリア -----------------------------
+    /**
+        @brief	実際に問題を解く関数
+        @note   マルチケースの場合はケースごとに呼ばれます
+     */
+    void Solve()
+    {
+        int64_t N, M;
+        In() >> N >> M;
+        std::vector<int64_t> L(N);
+        EachInput(L);
+
+        int64_t ans = BinarySearch(-1, 1ll << 60, [&](int64_t cur)
+                                   {
+            int curIdx = 0;
+            for(int m=0; m<M; ++m){
+                int64_t length = 0;
+                while(length < cur){
+                    if(cur - length >= L[curIdx] ){
+                        length += L[curIdx] + 1;
+                        curIdx++;
+                        if(curIdx == N){
+                            break;
+                        }
+                    }else{
+                        break;
+                    }
+                }
+                if(curIdx == N)
+                {
+                    break;
+                }
+            }
+            return curIdx == N; });
+
+        Out() << ans;
+    }
+
+    //----------- 以下編集の必要なし ----------------------
+
+public:
+    /**
+        @brief	各種処理の起点になる窓口
+     */
+    void Run()
+    {
+        int64_t numTestCases = 1;
+#ifdef ENABLE_MULTICASE
+        In() >> numTestCases;
+#endif
+        while (numTestCases--)
+        {
+            Solve();
+        }
+
+        std::cout << Out().str();
+    }
+
+private:
+    /**
+        @brief	対象とするコンテナの各要素に入力を受け取る
+
+        @tparam	TargetContainerType 対象とするコンテナの型
+        @param[in]	targetContainer 対象とするコンテナ
+     */
+    template <class TargetContainerType>
+    void EachInput(TargetContainerType &targetContainer)
+    {
+        for (auto &&target : targetContainer)
+        {
+            In() >> target;
+        }
+    }
+
+    /**
+        @brief	入力ストリームを取得
+
+        @return 入力ストリーム
+     */
+    std::istream &In()
+    {
+        return std::cin;
+    }
+
+    /**
+        @brief	出力ストリームを取得
+
+        @return 出力ストリーム
+     */
+    std::stringstream &Out()
+    {
+        return m_outStream;
+    }
+
+    /**
+        @brief	エラー出力ストリームを取得
+
+        @return エラー出力ストリーム
+     */
+    std::ostream &Error()
+    {
+        return std::cerr;
+    }
+
+    /**
+        @brief	Yesと1行出力する
+     */
+    void Yes()
+    {
+        Out() << "Yes" << std::endl;
+    }
+
+    /**
+        @brief	Noと1行出力する
+     */
+    void No()
+    {
+        Out() << "No" << std::endl;
+    }
+
+    /**
+        @brief	渡されたbool値に応じてYesかNoを出力する
+
+        @param[in]	isYes Yesを出力するかどうか
+     */
+    void YesNo(bool isYes)
+    {
+        if (isYes)
+        {
+            Yes();
+        }
+        else
+        {
+            No();
+        }
+    }
+
+private:
+    std::stringstream m_outStream; //!< 出力を一時的に貯めておけるストリーム
+};
+
+/**
+    @brief	エントリポイント
+
+    @return ステータスコード(atcoderにおいては基本0)
+ */
+int main()
+{
+    AtcoderSolveHelper solver;
+    solver.Run();
+    return 0;
+}
