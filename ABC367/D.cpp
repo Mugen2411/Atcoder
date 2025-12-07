fd0951b7dd9d391baa05e430630b8926a8a43014
Mugen2411 <shojirintaro@gmail.com>
2025-12-07 22:41:58 +0900

    train: ABC367-D

--- ABC367/D.cpp
+++ ABC367/D.cpp
@@ -1,27 +1,168 @@
 ﻿#include <iostream>
+#include <sstream>
+#include <string>
+#include <unordered_map>
 #include <vector>
 
+// #define ENABLE_MULTICASE //!< マルチケース用スイッチ：コメントを外すとマルチケースになる
+
+/**
+    @brief	Atcoderの解答を行うのに便利なクラス
+ */
+class AtcoderSolveHelper
+{
+  private:
+    //----------- 編集エリア -----------------------------
+    /**
+        @brief	実際に問題を解く関数
+        @note   マルチケースの場合はケースごとに呼ばれます
+     */
+    void Solve()
+    {
+        int64_t N, M;
+        In() >> N >> M;
+        std::vector<int64_t> A(N);
+        EachInput(A);
+
+        std::vector<int64_t> pres;
+        {
+            int64_t acm = 0;
+            for (int i = 0; i < N * 2 - 1; ++i)
+            {
+                acm += A[i % N];
+                pres.push_back(acm);
+            }
+        }
+
+        std::unordered_map<int64_t, int64_t> window;
+        for (int i = 0; i < N - 1; ++i)
+        {
+            ++window[pres[i] % M];
+        }
+
+        int64_t ans = 0;
+        for (int i = N - 1; i < N * 2 - 1; ++i)
+        {
+            ++window[pres[i] % M];
+            int64_t curMod = pres[i - N + 1] % M;
+            ans += window[curMod] - 1;
+            --window[curMod];
+        }
+
+        Out() << ans;
+    }
+
+    //----------- 以下編集の必要なし ----------------------
+
+  public:
+    /**
+        @brief	各種処理の起点になる窓口
+     */
+    void Run()
+    {
+        int64_t numTestCases = 1;
+#ifdef ENABLE_MULTICASE
+        In() >> numTestCases;
+#endif
+        while (numTestCases--)
+        {
+            Solve();
+        }
+
+        std::cout << Out().str();
+    }
+
+  private:
+    /**
+        @brief	対象とするコンテナの各要素に入力を受け取る
+
+        @tparam	TargetContainerType 対象とするコンテナの型
+        @param[in]	targetContainer 対象とするコンテナ
+     */
+    template <class TargetContainerType>
+    void EachInput(TargetContainerType &targetContainer)
+    {
+        for (auto &&target : targetContainer)
+        {
+            In() >> target;
+        }
+    }
+
+    /**
+        @brief	入力ストリームを取得
+
+        @return 入力ストリーム
+     */
+    std::istream &In()
+    {
+        return std::cin;
+    }
+
+    /**
+        @brief	出力ストリームを取得
+
+        @return 出力ストリーム
+     */
+    std::stringstream &Out()
+    {
+        return m_outStream;
+    }
+
+    /**
+        @brief	エラー出力ストリームを取得
+
+        @return エラー出力ストリーム
+     */
+    std::ostream &Error()
+    {
+        return std::cerr;
+    }
+
+    /**
+        @brief	Yesと1行出力する
+     */
+    void Yes()
+    {
+        Out() << "Yes" << std::endl;
+    }
+
+    /**
+        @brief	Noと1行出力する
+     */
+    void No()
+    {
+        Out() << "No" << std::endl;
+    }
+
+    /**
+        @brief	渡されたbool値に応じてYesかNoを出力する
+
+        @param[in]	isYes Yesを出力するかどうか
+     */
+    void YesNo(bool isYes)
+    {
+        if (isYes)
+        {
+            Yes();
+        }
+        else
+        {
+            No();
+        }
+    }
+
+  private:
+    std::stringstream m_outStream; //!< 出力を一時的に貯めておけるストリーム
+};
+
+/**
+    @brief	エントリポイント
+
+    @return ステータスコード(atcoderにおいては基本0)
+ */
 int main()
 {
-    size_t N, M;
-    std::cin >> N >> M;
-    std::vector<size_t> A(N * 2 + 1, 0);
-    for (int i = 1; i <= N; ++i)
-    {
-        std::cin >> A[i];
-        A[i + N] = A[i];
-        A[i] += A[i - 1];
-        A[i] %= M;
-    }
-    for (int i = N + 1; i <= 2 * N; ++i)
-    {
-        A[i] += A[i - 1];
-        A[i] %= M;
-    }
-    size_t ans = 0;
-    for (int i = 1; i <= N; ++i)
-    {
-        }
-    std::cout << ans;
+    AtcoderSolveHelper solver;
+    solver.Run();
     return 0;
 }
