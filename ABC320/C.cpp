8a9a32d7ef1682acb33746e0dadc255f9c4e8968
Mugen2411 <shojirintaro@gmail.com>
2025-12-07 16:41:50 +0900

    train: ABC320-C

---
+++ ABC320/C.cpp
@@ -0,0 +1,181 @@
+﻿#include <algorithm>
+#include <iostream>
+#include <set>
+#include <sstream>
+#include <string>
+
+// #define ENABLE_MULTICASE //!< マルチケース用スイッチ：コメントを外すとマルチケースになる
+
+/**
+    @brief	Atcoderの解答を行うのに便利なクラス
+ */
+class AtcoderSolveHelper
+{
+  private:
+    //----------- 編集エリア -----------------------------
+    /**
+        @brief	実際に問題を解く関数
+        @note   マルチケースの場合はケースごとに呼ばれます
+     */
+    void Solve()
+    {
+        int64_t M;
+        In() >> M;
+        std::string S[3];
+        In() >> S[0] >> S[1] >> S[2];
+
+        for (int i = 0; i < 3; ++i)
+        {
+            S[i] += S[i];
+            S[i] += S[i];
+        }
+
+        std::sort(std::begin(S), std::end(S));
+
+        int64_t ans = M * 1000;
+
+        do
+        {
+            for (int i = 0; i < 10; ++i)
+            {
+
+                int64_t s = 0;
+                for (int64_t t = 0; t < M * 4; ++t)
+                {
+                    if (S[s][t] - '0' == i)
+                    {
+                        ++s;
+                    }
+                    if (s == 3)
+                    {
+                        ans = std::min(ans, t);
+                        break;
+                    }
+                }
+            }
+        } while (std::next_permutation(std::begin(S), std::end(S)));
+
+        if (ans == M * 1000)
+        {
+            Out() << -1 << std::endl;
+        }
+        else
+        {
+            Out() << ans << std::endl;
+        }
+    }
+
+    //----------- 以下編集の必要なし ----------------------
+
+  public:
+    /**
+        @brief	各種処理の起点になる窓口
+     */
+    void Run()
+    {
+        int64_t numTestCases = 1;
+#ifdef ENABLE_MULTICASE
+        In() >> numTestCases;
+#endif
+        while (numTestCases--)
+        {
+            Solve();
+        }
+
+        std::cout << Out().str();
+    }
+
+  private:
+    /**
+        @brief	対象とするコンテナの各要素に入力を受け取る
+
+        @tparam	TargetContainerType 対象とするコンテナの型
+        @param[in]	targetContainer 対象とするコンテナ
+     */
+    template <class TargetContainerType>
+    void EachInput(TargetContainerType &targetContainer)
+    {
+        for (auto &&target : targetContainer)
+        {
+            In() >> target;
+        }
+    }
+
+    /**
+        @brief	入力ストリームを取得
+
+        @return 入力ストリーム
+     */
+    std::istream &In()
+    {
+        return std::cin;
+    }
+
+    /**
+        @brief	出力ストリームを取得
+
+        @return 出力ストリーム
+     */
+    std::stringstream &Out()
+    {
+        return m_outStream;
+    }
+
+    /**
+        @brief	エラー出力ストリームを取得
+
+        @return エラー出力ストリーム
+     */
+    std::ostream &Error()
+    {
+        return std::cerr;
+    }
+
+    /**
+        @brief	Yesと1行出力する
+     */
+    void Yes()
+    {
+        Out() << "Yes" << std::endl;
+    }
+
+    /**
+        @brief	Noと1行出力する
+     */
+    void No()
+    {
+        Out() << "No" << std::endl;
+    }
+
+    /**
+        @brief	渡されたbool値に応じてYesかNoを出力する
+
+        @param[in]	isYes Yesを出力するかどうか
+     */
+    void YesNo(bool isYes)
+    {
+        if (isYes)
+        {
+            Yes();
+        }
+        else
+        {
+            No();
+        }
+    }
+
+  private:
+    std::stringstream m_outStream; //!< 出力を一時的に貯めておけるストリーム
+};
+
+/**
+    @brief	エントリポイント
+
+    @return ステータスコード(atcoderにおいては基本0)
+ */
+int main()
+{
+    AtcoderSolveHelper solver;
+    solver.Run();
+    return 0;
+}
