9e4a25197da7a60dbdb3a9c91f31947ebe4dfcfc
Mugen2411 <shojirintaro@gmail.com>
2026-02-18 13:18:33 +0900

    ABC445終了。pf1145のrt1010(+16)初の4桁！

--- ABC445/D.cpp
+++ ABC445/D.cpp
@@ -1,16 +1,16 @@
 ﻿#ifndef ___RANGE_SET___
 #define ___RANGE_SET___
 
-#include <set>
-#include <limits>
 #include <algorithm>
+#include <limits>
+#include <set>
 
 //! @brief	区間をsetで管理する奴
 //! @tparam	T 管理する型(整数型のみ)
 template <class T>
 class RangeSet
 {
-public:
+  public:
     //! @brief	区間(閉区間)
     struct Range
     {
@@ -42,11 +42,11 @@ public:
         }
     };
 
-public:
+  public:
     static const T INF;               //!< 無限大
     static const Range INVALID_RANGE; //!< 無効区間
 
-public:
+  public:
     //! @brief デフォルトコンストラクタ
     RangeSet()
     {
@@ -191,7 +191,7 @@ public:
         return sumErased;
     }
 
-private:
+  private:
     //! @brief 指定した点から最も近い位置にある範囲を取得
     //! @param pivot 指定した点
     //! @return 最も近い範囲を指すイテレータ
@@ -200,7 +200,7 @@ private:
         return std::prev(m_rangeSet.lower_bound(Range{.Left = pivot, .Right = pivot}));
     }
 
-private:
+  private:
     std::set<Range> m_rangeSet; //!< 区間が入るコンテナ
 };
 
@@ -210,7 +210,7 @@ const T RangeSet<T>::INF = std::numeric_limits<T>::max() / 2;
 template <class T>
 const typename RangeSet<T>::Range RangeSet<T>::INVALID_RANGE = {.Left = INF, .Right = INF};
 
-#endif //___RANGE_SET___ 
+#endif //___RANGE_SET___
 #ifndef ___INCLUDED_BINARY_SEARCH___
 #define ___INCLUDED_BINARY_SEARCH___
 
@@ -236,7 +236,7 @@ int64_t BinarySearch(int64_t ng, int64_t ok, F comp)
     return ok;
 }
 
-#endif //___INCLUDED_BINARY_SEARCH___ 
+#endif //___INCLUDED_BINARY_SEARCH___
 #ifndef ___INCLUDED_BITMAN___
 #define ___INCLUDED_BITMAN___
 
@@ -527,7 +527,7 @@ struct hash<BitManager>
 } // namespace std
 
 #endif //___INCLUDED_BITMAN___
- 
+
 #ifndef ___INCLUDED_FRACTION___
 #define ___INCLUDED_FRACTION___
 
@@ -800,7 +800,7 @@ class Fraction
     int64_t m_numerator;   //!< 分子
 };
 
-#endif //___INCLUDED_FRACTION___ 
+#endif //___INCLUDED_FRACTION___
 #ifndef ___INCLUDED_SEGMENT_TREE___
 #define ___INCLUDED_SEGMENT_TREE___
 
@@ -1020,14 +1020,13 @@ class SegmentTree
 template <class T>
 const size_t SegmentTree<T>::INVALID = std::numeric_limits<size_t>::max();
 
-#endif //___INCLUDED_SEGMENT_TREE___ 
+#endif //___INCLUDED_SEGMENT_TREE___
 #ifndef ___INCLUDED_UNIONFIND___
 #define ___INCLUDED_UNIONFIND___
 
 #include <cstddef>
 #include <cstdint>
 #include <vector>
-
 
 /**
     @brief        UnionFind
@@ -1119,7 +1118,7 @@ class UnionFind final
 };
 
 #endif //___INCLUDED_UNIONFIND___
- 
+
 #ifndef ___INCLUDED_COMBINATION___
 #define ___INCLUDED_COMBINATION___
 
@@ -1142,7 +1141,7 @@ class Combination
     }
 };
 
-#endif //___INCLUDED_COMBINATION___ 
+#endif //___INCLUDED_COMBINATION___
 #ifndef __INCLUDED_GRID2D__
 #define __INCLUDED_GRID2D__
 
@@ -1416,7 +1415,7 @@ struct hash<Grid2D<T>>
 };
 } // namespace std
 
-#endif //__INCLUDED_GRID2D__ 
+#endif //__INCLUDED_GRID2D__
 #ifndef ___INCLUDED_PREFIX_SUM___
 #define ___INCLUDED_PREFIX_SUM___
 
@@ -1470,7 +1469,7 @@ class PrefixSum
     std::vector<T> m_data; //!< データ構造
 };
 
-#endif //___INCLUDED_PREFIX_SUM___ 
+#endif //___INCLUDED_PREFIX_SUM___
 #ifndef ___INCLUDED_MAIN___
 #define ___INCLUDED_MAIN___
 
@@ -1609,9 +1608,73 @@ int main()
 }
 
 //----------------------編集スペースここから--------------------------
+#include <unordered_map>
 
 void AtcoderSolveHelper::Solve()
 {
+    struct PIECE
+    {
+        int64_t H, W, Idx;
+    };
+    int64_t H, W, N;
+    In() >> H >> W >> N;
+    std::vector<PIECE> piece(N);
+    std::unordered_map<int64_t, std::set<int64_t>> vert;
+    std::unordered_map<int64_t, std::set<int64_t>> horiz;
+    for (int i = 0; i < N; ++i)
+    {
+        auto &p = piece[i];
+        In() >> p.H >> p.W;
+        p.Idx = i;
+        vert[p.H].insert(i);
+        horiz[p.W].insert(i);
+    }
+    struct POS
+    {
+        int64_t X, Y;
+        int64_t Idx;
+    };
+    POS cur = {0, 0};
+    std::vector<POS> ans;
+    while (!vert.empty())
+    {
+        if (vert.count(H - cur.Y))
+        {
+            for (auto &i : vert[H - cur.Y])
+            {
+                ans.push_back({cur.X, cur.Y, i});
+                horiz[piece[i].W].erase(i);
+                if (horiz[piece[i].W].empty())
+                {
+                    horiz.erase(piece[i].W);
+                }
+                cur.X += piece[i].W;
+            }
+            vert.erase(H - cur.Y);
+        }
+        else
+        {
+            if (horiz.count(W - cur.X))
+            {
+                for (auto &i : horiz[W - cur.X])
+                {
+                    ans.push_back({cur.X, cur.Y, i});
+                    vert[piece[i].H].erase(i);
+                    if (vert[piece[i].H].empty())
+                    {
+                        vert.erase(piece[i].H);
+                    }
+                    cur.Y += piece[i].H;
+                }
+                horiz.erase(W - cur.X);
+            }
+        }
+    }
+    std::sort(ans.begin(), ans.end(), [](const POS &lhs, const POS &rhs) { return lhs.Idx < rhs.Idx; });
+    for (auto &a : ans)
+    {
+        Out() << a.Y + 1 << " " << a.X + 1 << std::endl;
+    }
 }
 
 //----------------------編集スペースここまで--------------------------
