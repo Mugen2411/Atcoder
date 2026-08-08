ad8e0f4928b8b7adb7fd86c1a7a7dee2decda892
Mugen2411_A10Defect <shojirintaro@gmail.com>
2026-08-08 20:56:02 +0900

    add: ファイル追加

---
+++ ABC470/B.cpp
@@ -0,0 +1,13 @@
+#ifndef ___INCLUDED_MAIN___
+#define ___INCLUDED_MAIN___
+
+//#define ENABLE_MULTICASE //!< マルチケース用スイッチ：マルチケースの場合はコメント解除
+
+#include "../module/Helper.cpp"
+
+void AtcoderSolveHelper::Solve()
+{
+    return;
+}
+
+#endif //___INCLUDED_MAIN___
